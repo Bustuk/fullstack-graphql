c8a06101147bf9bf320b712ab358730ffff57892
Łukasz Wołodkiewicz <wolodkiewiczlukasz@gmail.com>
2022-12-08 10:00:16 +0100

    Query type solution

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,23 @@
 
 module.exports = {
   Query: {
-    
+    pets(a, b, ctx) {
+      console.log(ctx)
+      return ctx.models.Pet.findMany()
+    },
   },
-  Mutation: {
+  // Mutation: {
     
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
-    }
-  },
-  User: {
-    
-  }
+  // },
+  // Pet: {
+  //   img(pet) {
+  //     return pet.type === 'DOG'
+  //       ? 'https://placedog.net/300/300'
+  //       : 'http://placekitten.com/300/300'
+  //   },
+  //   pet: (_, {input}, {x}) => models.Pet.findOne(input),
+  // },
+  // User: {
+  //   user: (_, __, {x}) => models.User.findOne()
+  // }
 }
